a75cfca057afa78b0b939cc43f24153201f0d03a
Michael Byalsky <michael.byalsky@gmail.com>
2020-08-26 19:59:11 +0300

    Update myTests.test.js

--- client/src/myTests.test.js
+++ client/src/myTests.test.js
@@ -4,29 +4,7 @@
 const puppeteer = require("puppeteer");
 const fs = require('fs').promises
 
-// describe('My test', () => {
-//   test('check if the done button changes the status of the ticket (server needed)', async () => {
-//     const data = await fs.readFile('../server/data.json');
-//     const parsedData = JSON.parse(data);
-//     let browser = await puppeteer.launch({ args: [
-//       `--window-size=1280,768`,
-//     ], headless: false, slowMo: 100
-//     });
-//     let page = await browser.newPage();
-//     await page.goto('http://localhost:3000/');
-//     await page.waitForSelector('#doneButton_0', {visible: true});
-//     const button = await page.$('#doneButton_0');
-//     await button.click();
-//     setTimeout( async () => {
-//       const updateData  = await fs.readFile('../server/data.json')
-//       const parsedUpdatedData = JSON.parse(updateData)
-//       expect(parsedUpdatedData[0].updated).toBe(!parsedData[0].updated)
-//       browser.close()
-//     }, 5000);
-//   }, 50000)
-// })
-
-describe('My test', () => {
+describe('My test (only work with server)', () => {
   test('check if can order by status (done or undone)', async () => {
     let browser = await puppeteer.launch({ args: [
       `--window-size=1280,768`,
