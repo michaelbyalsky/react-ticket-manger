2e7897cfc55aa507b1e39b4c32e4d9e89ee3ddf4
Michael Byalsky <michael.byalsky@gmail.com>
2020-08-26 15:49:05 +0300

    eslint

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 // import logo from "./logo.svg";
 // import Ticket from './components/Ticket'
-import './App';
+import './App'; 
 import Main from '../Main/Main'
 
 function App() {
